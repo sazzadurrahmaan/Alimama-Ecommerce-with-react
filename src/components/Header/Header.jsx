3231a91482921c9c4ae654632805dd3b13cfffb4
sazzadurrahman-cse <sazzadrn17@gmail.com>
2023-08-14 12:18:11 +0600

    ali mama navbar created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css';
+import logo from '../../assets/images/alimama.png'
+const Header = () => {
+    return (
+        <>
+           <nav className="header">
+            <div className="left-nav">
+            <img src={logo} alt="" />
+            </div>
+            <div className="right-nav">
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/inventory">Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+           </nav>
+               
+        </>
+    );
+};
+
+export default Header;
