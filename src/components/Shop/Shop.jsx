d07fd1bc946cd0c66c6bc1895bea020b07767b99
sazzadurrahman-cse <sazzadrn17@gmail.com>
2023-08-17 00:38:03 +0600

    get cart and products from localstorage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -23,8 +23,18 @@ const Shop = () => {
 
     useEffect(()=>{
         const storedCart = getCartItem();
-        console.log(storedCart);
-    },[])
+        //step -1: get id
+        for(const id in storedCart){
+            //step -2 :get the product using id;
+            const addedProduct = products.find(product => product.id ===id);
+       // console.log(storedProduct)
+       //step -3 :quantity of the product
+
+       const quantity = storedCart[id];
+       addedProduct.quantity= quantity;
+       console.log(quantity);
+        }
+    },[products])
 
     return (
         <>
